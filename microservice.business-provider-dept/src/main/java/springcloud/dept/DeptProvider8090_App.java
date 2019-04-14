185d50ad6db7233c4f39a1805cd83d4f5d759489
xuf <xuf_001@126.com>
2019-04-14 10:06:55 +0800

    add cloud and configure cluster

--- microservice.business-provider-dept/src/main/java/springcloud/dept/DeptProvider8090_App.java
+++ microservice.business-provider-dept/src/main/java/springcloud/dept/DeptProvider8090_App.java
@@ -2,8 +2,10 @@ package springcloud.dept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8090_App {
 
 	public static void main(String[] args) {
